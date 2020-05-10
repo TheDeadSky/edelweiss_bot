b03301ef0dc94f544596190ea3f184f70295beea
DeadSky <deadsky.1136@gmail.com>
2020-05-11 00:34:04 +0300

    Try new youtube_dl args

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def play(ctx, url):
     ytdl = youtube_dl.YoutubeDL(YTDL_OPTS)
     info = ytdl.extract_info(url, download=False)
     
-    asrc = discord.PCMAudio(info['formats'][0]['url'])
+    asrc = discord.FFmpegPCMAudio(info['formats'][0]['url'])
 
     voice_client.play(asrc)
 
